dd989a28bd4bd96764acc3f4a201d35000114acf
Barbara Friščić <80264267+BarbaraFriscic@users.noreply.github.com>
2022-11-07 20:14:03 +0100

    Add project files.

---
+++ 7_11_22_Predavanje/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7_11_22_Predavanje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            Posao p1 = new Posao();
+            p1.OpisPosla = "pranje prozora";
+            p1.PlacaPoSatu = 20.00;
+            p1.BrojSati = 4.5;
+
+            Posao p2 = new Posao();
+            p2.OpisPosla = "anketiranje";
+            p2.PlacaPoSatu = 14.50;
+            p2.BrojSati = 8;
+
+            Posao p3 = new Posao();
+            p3 = p1 + p2;
+
+
+            Ispis(p1);
+            Ispis(p2);
+            Ispis(p3);
+             
+
+        }
+        void Ispis(Posao p)
+        {
+
+            label1.Text += String.Format("\nPosao: {0}, je plaćen {1:C} po satu za {2} sati rada.\nUkupno: {3:C}", p.OpisPosla, p.PlacaPoSatu, p.BrojSati, p.UkupnaPlaca);
+
+
+
+        }
+    }
+}
---
+++ 7_11_22_Predavanje/Posao.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_11_22_Predavanje
+{
+    internal class Posao
+    {
+		public string opisPosla;
+
+		public string OpisPosla
+		{
+			get { return opisPosla; }
+			set { opisPosla = value; }
+		}
+
+		public double brojSati;
+
+		public double BrojSati
+		{
+			get { return brojSati; }
+			set { 
+				brojSati = value;
+				ukupnaPlaca = placaPoSatu * brojSati;
+			}
+
+		}
+		private double placaPoSatu;
+
+		public double PlacaPoSatu
+		{
+			get { return placaPoSatu; }
+			set { placaPoSatu = value; }
+		}
+		private double ukupnaPlaca;
+
+		public double UkupnaPlaca
+		{
+			get { return ukupnaPlaca; }
+			
+		}
+		public static Posao operator +(Posao p1, Posao p2)
+		{
+			Posao noviPosao = new Posao();
+			noviPosao.opisPosla = p1.opisPosla + "i" + p2.opisPosla;
+			noviPosao.brojSati = p1.brojSati + p2.brojSati;
+			noviPosao.placaPoSatu = (p1.placaPoSatu + p2.placaPoSatu)/2;
+			noviPosao.ukupnaPlaca = noviPosao.placaPoSatu * noviPosao.brojSati;
+			return noviPosao;
+		}
+
+
+
+
+	}
+}
